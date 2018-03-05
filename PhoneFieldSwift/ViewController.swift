c55e00a31c17c2a9a963889e57b3e648f7836b26
abner <abner009007@users.noreply.github.com>
2018-03-05 16:07:18 +0800

    手机号输入框
    PhoneField是自动格式化手机号的输入框,支持纯文使用和xib使用

---
+++ PhoneFieldSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhoneFieldSwift
+//
+//  Created by 云中科技 on 2018/3/5.
+//  Copyright © 2018年 云中科技. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var phoneTextField: PhoneField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
